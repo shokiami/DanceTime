ee8be941c0468586d394f9cf85286b12d8c7fbb7
shokiami <kiami.sho@gmail.com>
2021-09-09 14:39:38 -0700

    Fixed stepping out of frame bug.

--- main.cc
+++ main.cc
@@ -1,4 +1,3 @@
-#include <thread>
 #include "pose.h"
 #include "canvas.h"
 
@@ -6,16 +5,12 @@ int main() {
   cv::VideoCapture capture(0);
   cv::Mat frame;
   PoseEstimator pose_estimator;
-  Pose pose;
   Canvas canvas;
   while ((cv::waitKey(1) & 0xFF) != 'q') {
     capture.read(frame);
     if (!frame.empty()) {
       cv::flip(frame, frame, 1);
-      Pose result = pose_estimator.estimate(frame);
-      if (!result.isEmpty()) {
-        pose = result;
-      }
+      Pose pose = pose_estimator.estimate(frame);
       canvas.renderPose(frame, pose);
       cv::imshow("DanceTime", frame);
     }
