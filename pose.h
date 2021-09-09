f2a40545a2c363133f74b494333646194be9edc7
shokiami <kiami.sho@gmail.com>
2021-09-08 20:26:43 -0700

    Tweaked min visibility and presence.

--- pose.h
+++ pose.h
@@ -42,8 +42,8 @@ class Landmark {
   cv::Point3d position;
   double visibility;
   double presence;
-  static constexpr double min_visibility = 0.5;
-  static constexpr double min_presence = 0.5;
+  static constexpr double min_visibility = 0.4;
+  static constexpr double min_presence = 0.1;
 };
 
 #endif
