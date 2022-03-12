d6f4510472d5897a0be101100d1cfd9c2832fb77
shokiami <kiami.sho@gmail.com>
2022-03-12 12:57:46 -0800

    Fixed bug.

--- main.cc
+++ main.cc
@@ -3,17 +3,47 @@
 #include "video.h"
 
 int main() {
-  Video video("blackpink");
+  cv::VideoCapture capture(0);
+  cv::Mat frame;
+  PoseEstimator pose_estimator;
   Canvas canvas;
-  video.play();
-  while (!video.finished()) {
-    cv::Mat frame = video.getFrame();
-    Pose pose = video.getPose();
-    canvas.renderPose(frame, pose);
-    cv::imshow("DanceTime", frame);
-    cv::waitKey(1);
+  while ((cv::waitKey(1) & 0xFF) != 'q') {
+    capture.read(frame);
+    if (!frame.empty()) {
+      cv::flip(frame, frame, 1);
+      Pose pose = pose_estimator.getPose(frame);
+      canvas.renderPose(frame, pose);
+      cv::imshow("DanceTime", frame);
+    }
   }
   return EXIT_SUCCESS;
+
+  // cv::VideoCapture capture(0);
+  // PoseEstimator pose_estimator;
+  // Canvas canvas;
+
+  // Video video("blackpink");
+  // video.play();
+
+  // while (!video.finished()) {
+  //   cv::Mat camera_frame;
+  //   capture.read(camera_frame);
+  //   if (!camera_frame.empty()) {
+  //     cv::flip(camera_frame, camera_frame, 1);
+  //     Pose camera_pose = pose_estimator.getPose(camera_frame);
+  //     canvas.renderPose(camera_frame, camera_pose);
+
+  //     // cv::Mat video_frame;
+  //     // video_frame = video.getFrame();
+  //     // Pose video_pose = video.getPose();
+  //     // canvas.renderPose(video_frame, video_pose);
+
+  //     // cv::hconcat(video_frame, camera_frame);
+  //     cv::imshow("DanceTime", camera_frame);
+  //     cv::waitKey(1);
+  //   }
+  // }
+  // return EXIT_SUCCESS;
 }
 
 // cv::VideoCapture capture(0);
