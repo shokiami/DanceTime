98a88da4ab0e5fd8e4b191bdba490f02917fb9c1
shokiami <kiami.sho@gmail.com>
2022-03-14 01:28:48 -0700

    Improved FPS counter in test.py.

--- test.py
+++ test.py
@@ -3,22 +3,16 @@ import time
 
 cap = cv2.VideoCapture("blackpink.mp4")
 start = time.time_ns()
-prev = start
+now = start
 curr = 0
-i = 0
 fps = cap.get(5)
 while(cap.isOpened()):
   now = time.time_ns()
   index = int(fps * (now - start) * 1e-9)
-  while (curr < index - 1):
+  for i in range(index - curr - 1):
     cap.grab()
-    curr += 1
   ret, frame = cap.read()
-  curr += 1
+  curr = index
   cv2.imshow("test", frame)
   cv2.waitKey(1)
-  if now - prev > 1000000000:
-    prev = now
-    print(i)
-    i = 0
-  i += 1
+  print(int(1e9 / (time.time_ns() - now)))
