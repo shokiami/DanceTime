3da38721652f53692090913b258f69b259ae1d81
shokiami <kiami.sho@gmail.com>
2022-12-27 10:54:15 -0800

    better resizing constants

--- src/game.cc
+++ src/game.cc
@@ -18,21 +18,26 @@ void Game::update() {
     ERROR("empty frame from camera");
   }
   cv::flip(camera_frame, camera_frame, 1);
+
   // get video frame
   video_frame = video.currFrame();
   if (video_frame.empty()) {
     ERROR("empty frame from video");
   }
+
   // get footer frame
   footer_frame = video.currFooterFrame();
   if (footer_frame.empty()) {
     ERROR("empty frame from video");
   }
+
   // get poses
   player_pose = pose_estimator.getPose(camera_frame);
   avatar_pose = video.getPose();
+
   // update audio
   audio.update();
+
   // update score
   scorer.add(player_pose, avatar_pose);
   TimePoint curr_time = std::chrono::steady_clock::now();
@@ -44,6 +49,7 @@ void Game::update() {
     // print fps and score
     cout << "fps: " << fps << ", score: " << score << endl;
   }
+
   // update fps
   elapsed_time = std::chrono::duration_cast<std::chrono::nanoseconds>(curr_time - prev_fps_time).count() * 1e-9;
   prev_fps_time = curr_time;
@@ -51,22 +57,30 @@ void Game::update() {
 }
 
 void Game::render() {
-  double video_scalar = camera_frame.rows / ((double) video_frame.cols / footer_frame.cols * footer_frame.rows + video_frame.rows);
-  double footer_scalar = video_scalar * video_frame.cols / footer_frame.cols;
-  // render player's pose
+  // prepare resizing constants
+  const double video_scalar = camera_frame.rows / ((double) footer_frame.rows * video_frame.cols / footer_frame.cols + video_frame.rows);
+  const int video_height = video_scalar * video_frame.rows;
+  const int video_width = video_scalar * video_frame.cols;
+  const int camera_height = camera_frame.rows;
+  const int camera_width = 1.5 * video_width;
+  const int footer_height = camera_height - video_height;
+  const int footer_width = video_width;
+
+  // crop camera frame
   // canvas.render(camera_frame, player_pose, 75, 125, 255);
-  camera_frame = camera_frame(cv::Range(0, camera_frame.rows),
-    cv::Range(0.5 * camera_frame.cols - 0.75 * video_scalar * video_frame.cols,
-    0.5 * camera_frame.cols + 0.75 * video_scalar * video_frame.cols));
-  // render avatar's pose
-  cv::resize(video_frame, video_frame, cv::Size(video_scalar * video_frame.cols, video_scalar * video_frame.rows));
+  camera_frame = camera_frame(cv::Range(0, camera_height),
+    cv::Range(0.5 * camera_frame.cols - 0.5 * camera_width, 0.5 * camera_frame.cols + 0.5 * camera_width));
+
+  // resize video frame and avatar pose
+  cv::resize(video_frame, video_frame, cv::Size(video_width, video_height));
   for (string body_part : avatar_pose.keys()) {
     avatar_pose[body_part] *= video_scalar;
   }
   // canvas.render(video_frame, avatar_pose, 255, 0, 255);
-  // resize footer
-  int adjustment = camera_frame.rows - (footer_scalar * footer_frame.rows + video_frame.rows) + 0.5;
-  cv::resize(footer_frame, footer_frame, cv::Size(footer_scalar * footer_frame.cols, footer_scalar * footer_frame.rows + adjustment));
+
+  // resize foot frame
+  cv::resize(footer_frame, footer_frame, cv::Size(footer_width, footer_height));
+
   // concatenate and display frames
   cv::Mat frame;
   cv::vconcat(video_frame, footer_frame, frame);
